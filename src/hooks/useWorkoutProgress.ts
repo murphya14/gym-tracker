ce02f50470fe233bb8679e666db5d6ec92d5bb95
murphya14 <50335453+murphya14@users.noreply.github.com>
2026-05-23 12:47:46 +0100

    fix workout progress prisma type

--- src/hooks/useWorkoutProgress.ts
+++ src/hooks/useWorkoutProgress.ts
@@ -1,87 +1,97 @@
-import { useState, useEffect, useCallback } from 'react';
-import { type Workout } from '@prisma/client';
+import { useState, useEffect, useCallback } from "react";
+import { type WorkoutPlan } from "@prisma/client";
 
 export type WorkoutCompletionMap = Record<string, boolean>;
 
-export const useWorkoutProgress = (workouts: Workout[] | undefined) => {
-	const [workoutProgressMap, setWorkoutProgressMap] =
-		useState<WorkoutCompletionMap>({});
-	const [isEveryWorkoutComplete, setIsEveryWorkoutComplete] =
-		useState<boolean>(false);
+export const useWorkoutProgress = (
+  workouts: WorkoutPlan[] | undefined
+) => {
+  const [workoutProgressMap, setWorkoutProgressMap] =
+    useState<WorkoutCompletionMap>({});
 
-	const isValidMap = useCallback(
-		(map: WorkoutCompletionMap) => {
-			return (
-				Object.values(map).every(Boolean) &&
-				workouts?.length == Object.values(map).length
-			);
-		},
-		[workouts],
-	);
+  const [isEveryWorkoutComplete, setIsEveryWorkoutComplete] =
+    useState<boolean>(false);
 
-	useEffect(() => {
-		if (!workouts) return;
+  const isValidMap = useCallback(
+    (map: WorkoutCompletionMap) => {
+      return (
+        Object.values(map).every(Boolean) &&
+        workouts?.length === Object.values(map).length
+      );
+    },
+    [workouts]
+  );
 
-		const workoutCompletionMap = localStorage.getItem(
-			'workoutCompletionMap',
-		);
+  useEffect(() => {
+    if (!workouts) return;
 
-		if (workoutCompletionMap) {
-			const parsedMap = JSON.parse(
-				workoutCompletionMap,
-			) as WorkoutCompletionMap;
-			if (isValidMap(parsedMap)) {
-				setWorkoutProgressMap(parsedMap);
-				setIsEveryWorkoutComplete(
-					Object.values(parsedMap).every(Boolean),
-				);
-				return;
-			}
-		}
+    const workoutCompletionMap = localStorage.getItem(
+      "workoutCompletionMap"
+    );
 
-		const initialMap: WorkoutCompletionMap = Object.fromEntries(
-			workouts.map(({ id }) => [id, false]),
-		);
-		localStorage.setItem(
-			'workoutCompletionMap',
-			JSON.stringify(initialMap),
-		);
-		setWorkoutProgressMap(initialMap);
-		setIsEveryWorkoutComplete(false);
-	}, [isValidMap, workouts]);
+    if (workoutCompletionMap) {
+      const parsedMap = JSON.parse(
+        workoutCompletionMap
+      ) as WorkoutCompletionMap;
 
-	// Update workout progress
-	const updateWorkoutProgress = useCallback(
-		(workoutId: string, isComplete: boolean) => {
-			setWorkoutProgressMap((prevMap) => {
-				const updatedWorkouts = {
-					...prevMap,
-					[workoutId]: isComplete,
-				};
-				localStorage.setItem(
-					'workoutCompletionMap',
-					JSON.stringify(updatedWorkouts),
-				);
-				const allComplete =
-					Object.values(updatedWorkouts).every(Boolean);
-				setIsEveryWorkoutComplete(allComplete);
-				return updatedWorkouts; // Return the updated state
-			});
-		},
-		[],
-	);
+      if (isValidMap(parsedMap)) {
+        setWorkoutProgressMap(parsedMap);
 
-	// Reset workout progress
-	const resetWorkoutProgress = useCallback(() => {
-		localStorage.removeItem('workoutCompletionMap');
-		setWorkoutProgressMap({});
-		setIsEveryWorkoutComplete(false);
-	}, []);
+        setIsEveryWorkoutComplete(
+          Object.values(parsedMap).every(Boolean)
+        );
 
-	return {
-		workoutProgressMap,
-		isEveryWorkoutComplete,
-		updateWorkoutProgress,
-		resetWorkoutProgress,
-	};
+        return;
+      }
+    }
+
+    const initialMap: WorkoutCompletionMap = Object.fromEntries(
+      workouts.map(({ id }) => [id, false])
+    );
+
+    localStorage.setItem(
+      "workoutCompletionMap",
+      JSON.stringify(initialMap)
+    );
+
+    setWorkoutProgressMap(initialMap);
+    setIsEveryWorkoutComplete(false);
+  }, [isValidMap, workouts]);
+
+  const updateWorkoutProgress = useCallback(
+    (workoutId: string, isComplete: boolean) => {
+      setWorkoutProgressMap((prevMap) => {
+        const updatedWorkouts = {
+          ...prevMap,
+          [workoutId]: isComplete,
+        };
+
+        localStorage.setItem(
+          "workoutCompletionMap",
+          JSON.stringify(updatedWorkouts)
+        );
+
+        const allComplete =
+          Object.values(updatedWorkouts).every(Boolean);
+
+        setIsEveryWorkoutComplete(allComplete);
+
+        return updatedWorkouts;
+      });
+    },
+    []
+  );
+
+  const resetWorkoutProgress = useCallback(() => {
+    localStorage.removeItem("workoutCompletionMap");
+    setWorkoutProgressMap({});
+    setIsEveryWorkoutComplete(false);
+  }, []);
+
+  return {
+    workoutProgressMap,
+    isEveryWorkoutComplete,
+    updateWorkoutProgress,
+    resetWorkoutProgress,
+  };
 };
